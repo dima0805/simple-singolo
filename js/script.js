6e11c140d095507cf9e440b11eba940217a3aa62
Dmitry Avdeychik <dmitri0805@gmail.com>
2020-12-06 21:32:41 +0300

    refactor: html css js files

--- js/script.js
+++ js/script.js
@@ -1,17 +1,18 @@
-MENU.addEventListener("click", event => {
-    MENU.querySelectorAll("li").forEach(el => {
-      el.querySelector("a").classList.remove("active");
-      event.target.classList.add("active");
-    });
-  });
+const MENU = document.getElementById('menu');
 
 
+MENU.addEventListener('click', (event) => {
+  MENU.querySelectorAll('li').forEach(el => el.classList.remove('active'));
+  event.target.classList.add('active');
+})
+
+/*
 document.addEventListener('scroll', onscroll);
 
 function onScroll(event) {
     const curPos = window.scrollY;
     const divs = document.querySelectorAll('#wrapper>div');
-    const links = document.querySelectorAll('menu_nav a');
+    const links = document.querySelectorAll('.navigation a');
  divs.forEach((el) => {
     if (el.offsetTop <= curPos && (el.offsetTop +el.offsetHeight) > curPos) {
         links.forEach((a) => {
